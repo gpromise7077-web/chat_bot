1c8ec1b4f280749d2f534b99f5bd51876b22f9b8
gpromise7077-web <gpromise7077@gmail.com>
2026-06-26 05:54:55 -0400

    project update

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
+import "tailwindcss";
 import { FaArrowUp } from "react-icons/fa";
 import { SiChatbot } from "react-icons/si";
 
